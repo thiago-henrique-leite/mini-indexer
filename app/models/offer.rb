9462d3d57b237b15f251debddbd7bef020ab74e7
Thiago Henrique <thiago.leitesilva05@gmail.com>
2024-10-31 16:03:08 -0300

    feat(models): add course association to offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :university_offer
+  has_one :course, through: :university_offer
 
   validates :discount_percentage, presence: true
   validates :discount_percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
