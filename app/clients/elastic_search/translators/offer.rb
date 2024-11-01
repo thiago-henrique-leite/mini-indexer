9789960e0f8a4c73319fe5c0a598337be804d49c
Thiago Henrique <thiago.leitesilva05@gmail.com>
2024-11-01 09:40:01 -0300

    feat(clients): add elastic search offer translator

---
+++ app/clients/elastic_search/translators/offer.rb
@@ -0,0 +1,29 @@
+module ElasticSearch
+  module Translators
+    class Offer
+      def initialize(offer)
+        @offer = offer
+      end
+
+      def translate
+        {
+          offer_id: offer.id,
+          name: course.name,
+          level: course.level,
+          kind: course.kind,
+          shift: course.shift,
+          full_price: university_offer.full_price,
+          max_payments: university_offer.max_payments,
+          enrollment_semester: university_offer.enrollment_semester,
+          discount_percentage: offer.discount_percentage,
+        }
+      end
+
+      private
+
+      attr_reader :offer
+
+      delegate :university_offer, :course, to: :offer
+    end
+  end
+end
