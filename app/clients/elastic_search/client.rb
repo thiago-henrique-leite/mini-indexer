c363d88b3611d2a46e027c0d93735d9f9a20f5b7
Thiago Henrique <thiago.leitesilva05@gmail.com>
2024-11-01 09:38:25 -0300

    feat(clients): add elastic search client

---
+++ app/clients/elastic_search/client.rb
@@ -0,0 +1,25 @@
+module ElasticSearch
+  class Client
+    include Singleton
+
+    def create_documents(index, documents)
+      body = ElasticSearch::Translators::Documents.new(index, documents).translate
+
+      connection.bulk(body: body)
+    end
+
+    def search_documents(index, body)
+      connection.search(index: index, body: body)
+    end
+
+    def offer_translator(offer)
+      ElasticSearch::Translators::Offer.new(offer)
+    end
+
+    private
+
+    def connection
+      @connection ||= Elasticsearch::Client.new(host: Settings.elastic_search.host)
+    end
+  end
+end
