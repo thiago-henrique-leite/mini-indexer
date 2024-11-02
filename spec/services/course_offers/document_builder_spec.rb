66e3a30145647ce9fa0c2d9cbe6a59a45f60f549
Thiago Henrique <thiago.leitesilva05@gmail.com>
2024-11-02 12:41:00 -0300

    test(servic/course-offers): add document-builder spec

---
+++ spec/services/course_offers/document_builder_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe CourseOffers::DocumentBuilder do
+  describe '#build' do
+    let(:offer) { create(:offer) }
+    let(:course) { offer.course }
+    let(:university_offer) { offer.university_offer }
+
+    subject { described_class.new(offer).build }
+
+    it 'builds the document' do
+      expect(subject).to eq(
+        object_id: offer.id,
+        name: course.name,
+        level: course.level,
+        kind: course.kind,
+        shift: course.shift,
+        full_price: university_offer.full_price,
+        max_payments: university_offer.max_payments,
+        enrollment_semester: university_offer.enrollment_semester,
+        discount_percentage: offer.discount_percentage
+      )
+    end
+  end
+end
