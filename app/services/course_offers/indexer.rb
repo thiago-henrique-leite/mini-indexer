79d9d44850fb4e1fcf029590bc924d3746f1ba06
maiamss <maiara.ctt@gmail.com>
2024-11-08 13:15:18 -0300

    feat: create update_index

--- app/services/course_offers/indexer.rb
+++ app/services/course_offers/indexer.rb
@@ -7,28 +7,41 @@ module CourseOffers
     end
 
     def perform
-      offers = Offer.enabled.includes(:university_offer, :course)
-      build_documents(offers)
+
+      offer =  Offer.enabled.includes(:university_offer, :course)
+      build_documents(offer)
       index_documents
     end
 
-    def index_course
-      #buscar ofertas no bancdo
-      #buscar ofertas no indice
-      #comparar ofertas
-      #remover as ofertas desabilitadas
-      #indexar as ofertas novas
+
+    def update_index
+      offers = Offer.enabled.where(course_id: course_id).includes(:university_offer, :course)
+      build_documents(offers)
+
+      offer_ids = offer.pluck(:id)
+      indexed_offers_ids = client.instance.search_documents(INDEX_NAME, { query: { terms: { offer_id: offer_ids } } })
+
+      offers_to_index = offers.diference(indexed_offers_ids)
+      client.instance.delete_documents(INDEX_NAME, offer_ids)
+
+      index_documents
     end
 
     private
 
     attr_reader :client, :documents
 
-    def build_documents(offers)
+    def offers
+      Offer.enabled.includes(:university_offer, :course)
+    end
+
+
+    def build_documents(offer)
       @documents = offers.map do |offer|
         CourseOffers::DocumentBuilder.new(offer).build
       end
     end
+
 
     def index_documents
       client.instance.index_documents(INDEX_NAME, documents)
