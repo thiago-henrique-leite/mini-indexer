eb9ff399c2aaa3d6feb0544f33f015d0c6c7cea9
bl4ckserious <luccasfontes125@gmail.com>
2024-11-08 11:30:11 -0300

    feat(elastic-search/translators): add DeleteDocuments translator

---
+++ app/clients/elastic_search/translators/delete_documents.rb
@@ -0,0 +1,28 @@
+module ElasticSearch
+    module Translators
+      class DeleteDocuments
+        def initialize(index, documents)
+          @index = index
+          @documents = documents
+        end
+  
+        def translate
+          documents.flat_map do |document|
+            [
+              {
+                delete: {
+                  _index: index,
+                  _id: document[:object_id]
+                }
+              }
+            ].append(document)
+          end
+        end
+  
+        private
+  
+        attr_reader :index, :documents
+      end
+    end
+  end
+  
