d92f21e5f17405fe36853173b7743a8e22290076
alimkhodr <ali.m.khodr@gmail.com>
2024-11-28 09:45:29 -0300

    fix: update_index

--- app/services/course_offers/update_indexer.rb
+++ app/services/course_offers/update_indexer.rb
@@ -6,52 +6,53 @@ module CourseOffers
       @client = client
     end
 
-  def perform(course_id)
-    offers = search_offers_by_course(course_id)
-    update_index(offers)
-  end
-
-  def update_index(offers)
-    offer_ids = offers.pluck(:id)
-
-    indexed_offers = search_indexed_offers(offer_ids)
-
-    offers_to_index = offers.reject { |offer| indexed_offers.include?(offer.id) }
-    offers_to_delete = offers.select { |offer| !indexed_offers.include?(offer.id) }
-
-    delete_documents(offers_to_delete)
-
-    build_documents(offers_to_index)
-    index_documents
-
-    { removed_count: offers_to_delete.size, created_count: offers_to_index.size }
-  end
-
-  private
-
-  attr_reader :client
-
-  def search_offers_by_course(course_id)
-    Offer.enabled.where(course_id: course_id).includes(:university_offer, :course)
-  end
-
-  def search_indexed_offers(offer_ids)
-    index_documents = client.instance.search_documents(INDEX_NAME, { query: { terms: { offer_id: offer_ids } } })
-    index_documents.map { |doc| doc['id'] } # id das ofertas q ja estao indexadas?
-  end
-
-  def delete_documents(offers_to_delete)
-    offer_ids = offers_to_delete.map(&:id)
-
-    client.instance.delete_documents(INDEX_NAME, {query: { terms: { offer_id: offer_ids } }})
-  end
-
-  def build_documents(offers)
-    documents = offers.map do |offer|
-      CourseOffers::DocumentBuilder.new(offer).build
+    def perform(course_id)
+      offers = search_offers_by_course(course_id)
+      update_index(offers, course_id)
     end
+
+    def update_index(offers, course_id)
+      indexed_offers = search_indexed_offers(course_id)
+      list_offer = offers.pluck(:id)
+      @offers_to_index = offers.reject { |offer| indexed_offers.include?(offer.id) }
+      offers_to_delete = indexed_offers.select { |offer| !list_offer.include?(offer) }
+      delete_documents(offers_to_delete)
+      build_documents(offers_to_index)
+      index_documents
+      { removed_count: offers_to_delete.size, created_count: offers_to_index.size }
+    end
+
+    private
+
+    attr_reader :client
+
+    def search_offers_by_course(course_id)
+      teste = Offer.enabled.includes(:university_offer, :course).joins(:course).where(courses: { id: course_id })
+      puts teste
+      teste
+    end
+
+    def search_indexed_offers(course_id)
+      course_list = []
+      course_list << course_id
+      index_documents = client.instance.search_documents(INDEX_NAME, { query: { terms: { "course_id": course_list } } })
+      index_documents['hits']['hits'].map { |doc| doc['_id'].to_i }
+    end
+
+    def delete_documents(offer_ids)
+      client.instance.delete_documents(INDEX_NAME, {query: { terms: { offer_id: offer_ids } }})
+    end
+
+    def build_documents(offers)
+      documents = offers.map do |offer|
+        CourseOffers::DocumentBuilder.new(offer).build
+      end
+    end
+
+    def index_documents
+      client.instance.index_documents(INDEX_NAME, @offers_to_index)
+    end
+
   end
-  def index_documents
-    client.instance.index_documents(INDEX_NAME, documents)
-  end
+
 end
